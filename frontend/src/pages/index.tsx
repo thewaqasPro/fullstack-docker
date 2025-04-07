7966b4438f0961a8a42d6cc88f782d2a6da0e175
Waqas Asghar <thewaqaspro@gmail.com>
2025-04-08 00:19:44 +0500

    First Update in docker file

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -10,7 +10,7 @@ interface User {
 }
 
 export default function Home() {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000';
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'https://dy-api.waqas.pro';
   const [users, setUsers] = useState<User[]>([]);
   const [newUser, setNewUser] = useState({ name: '', email: '' });
   const [updateUser, setUpdateUser] = useState({ id: '', name: '', email: '' });
